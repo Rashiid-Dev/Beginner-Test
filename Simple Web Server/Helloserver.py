60b42955bb21bfe8f361b2e523bb58af49267e2d
Abdirashiid Jama <rashiid_jacfar@hotmail.com>
2018-10-24 11:28:51 +0100

    Made small changes to Helloserver.py

--- Simple Web Server/Helloserver.py
+++ Simple Web Server/Helloserver.py
@@ -5,13 +5,13 @@ class HelloHandler(BaseHTTPRequestHandler):
 		#sends a 200 OK response
 		self.send_response(200)
         #Sends Headers
-		self.send_jeader("content-type", "text/plain;utf-8")
+		self.send_header("Content-type", "text/plain; utf-8")
 		self.end_headers()
 		# Writes the response body
-		self.wfile.write("Hello, HTT!\n".encode())
+		self.wfile.write("Hello, HTTP!\n".encode())
 
-		if __name__ == "__main__":
-			server_address = ("", 8000) #Serve on all addresses, port 8000
-			httpd = HTTPServer(server_address, HelloHandler)
-			httpd.server_forever()
+if __name__ == "__main__":
+	server_address = ("", 8000) #Serve on all addresses, port 8000
+	httpd = HTTPServer(server_address, HelloHandler)
+	httpd.serve_forever()
 
